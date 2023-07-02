efac6b9c4c514621ecd3553a91e282a7002de731
azrarafeeq <azraaminah@gmail.com>
2023-07-02 22:30:35 +0400

    wordpress configuration

--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -d "/home/${USER}/data" ]; then
-        mkdir ~/data
-        mkdir ~/data/mariadb
-        mkdir ~/data/wordpress
+if [ ! -d "$PWD/srcs/data" ]; then
+        mkdir $PWD/srcs/data
+        mkdir $PWD/srcs/data/mariadb
+        mkdir $PWD/srcs/data/wordpress
 fi
