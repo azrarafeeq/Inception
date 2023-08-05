3cae2df0895844f52517b5b1b1e10831c7c72276
azrarafeeq <azraaminah@gmail.com>
2023-08-05 13:26:42 -0400

    wordpress

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,7 +2,7 @@
 
 wp --allow-root --path=/var/www core install \
   --url="$DOMAIN_NAME" \
-  --title="arafeeq Word-Press" \
+  --title="Azra Rafeeq's WordPress" \
   --admin_user="$WPADUSER" \
   --admin_password="$WPADPASS" \
   --admin_email="arafeeq@student.42abudhabi.ae"
@@ -12,7 +12,7 @@ wp user create "$WPUSER1" "user1@student.42abudhabi.ae" \
   --user_pass="$WPUSER1PASS" \
   --allow-root
 
-wp theme install twentytwentyone --activate --allow-root
+wp theme install twentytwenty --activate --allow-root
 
 wp plugin update --all --allow-root
 
